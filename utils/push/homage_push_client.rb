77e213e1e373fdd83acf2b81271985d3985da8c6
Nir Channes <nirh2@yahoo.com>
2015-04-02 21:34:24 +0300

    Cleaning the code and removing all usage of old push certificate

--- utils/push/homage_push_client.rb
+++ utils/push/homage_push_client.rb
@@ -3,7 +3,7 @@ module HomagePush
 	class Client
 		# apn (Apple Push Notification)
 		# gcm (Google Cloud Messaging)
-		attr_accessor :apn, :gcm, :apn_new
+		attr_accessor :apn, :gcm
 
 		class << self
 			def development
@@ -13,11 +13,9 @@ module HomagePush
 				homage_client.gcm = GCM.new("AIzaSyBLZSS5D3k07As3GS2HXKc8aMqV8xh5KSQ")
 				# Apple Push Notification
 				homage_client.apn = Houston::Client.development
-				homage_client.apn.certificate = File.read(File.expand_path("../../../certificates/homage_push_notification_dev.pem", __FILE__))
-				# Apple Push Notification - For new client (version 1.5.0 and up)
-				homage_client.apn_new = Houston::Client.production
-				homage_client.apn_new.certificate = File.read(File.expand_path("../../../certificates/homage_push_notification_prod_150.pem", __FILE__))
-				homage_client.apn_new.passphrase = "homage"
+				#homage_client.apn.certificate = File.read(File.expand_path("../../../certificates/homage_push_notification_dev.pem", __FILE__))
+				homage_client.apn.certificate = File.read(File.expand_path("../../../certificates/homage_push_notification_prod_150.pem", __FILE__))
+				homage_client.apn.passphrase = "homage"
 
 				# Monkey Push Client
 				monkey_client = self.new
@@ -39,13 +37,8 @@ module HomagePush
 
 				# Apple Push Notification
 				homage_client.apn = Houston::Client.production
-				homage_client.apn.certificate = File.read(File.expand_path("../../../certificates/homage_push_notification_prod.pem", __FILE__))
+				homage_client.apn.certificate = File.read(File.expand_path("../../../certificates/homage_push_notification_prod_150.pem", __FILE__))
 				homage_client.apn.passphrase = "homage"
-
-				# Apple Push Notification - For new client (version 1.5.0 and up)
-				homage_client.apn_new = Houston::Client.production
-				homage_client.apn_new.certificate = File.read(File.expand_path("../../../certificates/homage_push_notification_prod_150.pem", __FILE__))
-				homage_client.apn_new.passphrase = "homage"
 
 				# Monkey Push Client
 				monkey_client = self.new
@@ -66,8 +59,7 @@ module HomagePush
 			notification.alert = message
 			notification.custom_data = data
 			notification.sound = "default"
-			#@apn.push(notification)	
-			@apn_new.push(notification) if @apn_new
+			@apn.push(notification)	
 	    end
 
 	    def push_android(token, message ,data)
